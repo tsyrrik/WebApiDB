d9c92a6c9619e1afe9b454fc73334e0673ca695e
tsyren <https://github.com/tsyrrik/Lar-onlineShop>
2024-09-05 21:18:26 +0700

    add update

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/games', [GameController::class, 'index']);
 Route::get('/games/{id}', [GameController::class, 'show']);
 Route::post('/games', [GameController::class, 'store']);
